1a4ce49fa06ae8a522d3c67f9d13e9af241cb352
elCubil <rodriguez.arias.miguel@gmail.com>
2026-06-05 14:37:41 -0500

    se añadio tests individuales para asegurar se cumplan logicas de negocio

---
+++ dbt/tests/stg_fact_trip_test_total_sum.sql
@@ -0,0 +1,10 @@
+WITH tabla1 AS(
+
+    SELECT(fare_amount+extra+mta_tax+tip_amount
++tolls_amount+improvement_surcharge+congestion_surcharge+airport_fee) AS suma
+    FROM {{ref ('stg_fact_trip')}}
+)
+
+SELECT COUNT(*)
+FROM tabla1
+WHERE suma<=0
