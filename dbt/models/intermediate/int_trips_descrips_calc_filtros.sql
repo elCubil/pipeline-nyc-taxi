e4e0a3e628fa767483ecb7feebbfb5d05f569ea5
elCubil <rodriguez.arias.miguel@gmail.com>
2026-06-11 19:48:18 -0500

    se agrego modelo al schema intermediate

---
+++ dbt/models/intermediate/int_trips_descrips_calc_filtros.sql
@@ -0,0 +1,28 @@
+WITH t1 AS(
+
+    SELECT trip_id,ft.vendor_id,dv.vendor_name,tpep_pickup_datetime,tpep_dropoff_datetime,
+EXTRACT(EPOCH FROM (tpep_dropoff_datetime-tpep_pickup_datetime))/60 AS trip_duration,passenger_count,
+trip_distance,ft.ratecode_id,dr.ratecode_desc,store_and_fwd_flag,pu_location_id,dl1.zone AS partida,
+do_location_id,dl2.zone AS llegada,
+payment_type,dp.payment_desc,
+fare_amount,extra,mta_tax,tip_amount,tolls_amount,improvement_surcharge,congestion_surcharge,airport_fee,
+(fare_amount + extra+mta_tax+tip_amount+tolls_amount+improvement_surcharge+congestion_surcharge+airport_fee)AS total_amount
+
+    FROM {{ref('stg_fact_trip')}} ft
+        JOIN {{ref('stg_dim_vendor')}} dv ON ft.vendor_id=dv.vendor_id
+            JOIN {{ref('stg_dim_ratecode')}} dr ON ft.ratecode_id=dr.ratecode_id
+            JOIN {{ref('stg_dim_location')}} dl1 ON ft.pu_location_id=dl1.location_id
+            JOIN {{ref('stg_dim_location')}} dl2 ON ft.do_location_id=dl2.location_id
+            JOIN {{ref('stg_dim_payment_type')}} dp ON ft.payment_type=dp.payment_type_id
+
+    WHERE
+    (trip_distance>0)
+    AND
+    (tpep_dropoff_datetime-tpep_pickup_datetime)>INTERVAL '0'
+    AND
+    (fare_amount + extra+mta_tax+tip_amount+tolls_amount+improvement_surcharge+congestion_surcharge+airport_fee)>0
+)
+
+
+SELECT*
+FROM t1
